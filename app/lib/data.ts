e50380a03d5b522188fbd4372e800d83de5ff40d
ALI-ZIA-KHAN <azk443.443@gmail.com>
2024-02-05 00:12:52 +0500

    Tewelve Chapter Now

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -179,7 +179,7 @@ export async function fetchInvoiceById(id: string) {
   }
 }
 
-export async function fetchCustomers() {
+export async function   fetchCustomers() {
     noStore();
   try {
     const data = await sql<CustomerField>`
